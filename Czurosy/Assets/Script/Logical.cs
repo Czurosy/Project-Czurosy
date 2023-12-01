962abdf3e9171b9cc168ab93be6dcb47fd9ac92b
aniu <anna.czurak.08@gmail.com>
2023-12-01 20:21:49 +0100

    follow

---
+++ Czurosy/Assets/Script/Logical.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Logical : MonoBehaviour
+{
+    public GameObject enemy;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
