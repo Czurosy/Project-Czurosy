929512a4e6b8a41cd8a3dabff278ff7d24c6d95e
aniu <anna.czurak.08@gmail.com>
2023-12-02 00:21:47 +0100

    MODLITWA

--- Czurosy/Assets/Scripts/SpawningEnemy.cs
+++ Czurosy/Assets/Scripts/SpawningEnemy.cs
@@ -6,9 +6,13 @@ using UnityEngine;
 
 public class SpawningEnemy : MonoBehaviour
 {
-    public Transform player;
+    public GameObject player;
+    public GameObject enemy;
     private Rigidbody2D rb;
     public float speed;
+    private float time = 0.0f;
+    private float interpolationPeriod = 10.0f;
+
     private float distance;
     void Start()
     {
@@ -21,12 +25,22 @@ public class SpawningEnemy : MonoBehaviour
         distance = Vector2.Distance(transform.position, player.transform.position);
         Vector2 direction = player.transform.position - transform.position;
         direction.Normalize();
-        //float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
         if (distance < 20)
         {
             transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
-           // transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+            transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        }
+
+        time += Time.deltaTime;
+
+        if (time >= interpolationPeriod)
+        {
+            time = 0.0f;
+            float lowestPoint = transform.position.y - 30;
+            float highestPoint = transform.position.y + 30;
+            Instantiate(enemy, new Vector2(Random.Range(lowestPoint, highestPoint), Random.Range(lowestPoint, highestPoint)), transform.rotation);
         }
 
     }
