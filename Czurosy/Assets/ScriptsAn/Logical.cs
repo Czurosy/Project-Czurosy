c0ea04ff3860a54fca93f409e3f86c0974b4fe94
aniu <anna.czurak.08@gmail.com>
2023-12-02 03:14:52 +0100

    e

--- Czurosy/Assets/ScriptsAn/Logical.cs
+++ Czurosy/Assets/ScriptsAn/Logical.cs
@@ -5,10 +5,25 @@ using UnityEngine;
 
 public class Logical : MonoBehaviour
 {
+    private float time = 0.0f;
+    private float interpolationPeriod = 10.0f;
+    public GameObject enemy;
     public int fala;
+    public int[] numberOfZombie;
     void Start()
     {
-        fala = 1;
+        int[] numberOfZombie = { 6, 10, 16, 20, 30, 100 };
+        fala = 0;
+
+        time += Time.deltaTime;
+        if(fala != 5)
+        {
+            for (int i = 0; i < numberOfZombie[fala]; i++) {
+                float[] highestPoint = { -9.0f, 10.0f };
+                float[] lowestPoint = { -15.0f, 3.0f };
+                Instantiate(enemy, new Vector2(Random.Range(highestPoint[0], highestPoint[1]), Random.Range(lowestPoint[0], lowestPoint[1])), transform.rotation);
+            }
+        }
     }
 
 
