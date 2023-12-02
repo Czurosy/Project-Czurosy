147e78ae3c497b9abd7d0a6a2e420ef5347e621d
aniu <anna.czurak.08@gmail.com>
2023-12-02 02:57:33 +0100

    3

--- Czurosy/Assets/ScriptsAn/Logical.cs
+++ Czurosy/Assets/ScriptsAn/Logical.cs
@@ -5,26 +5,15 @@ using UnityEngine;
 
 public class Logical : MonoBehaviour
 {
-    private float time = 0.0f;
-    public float interpolationPeriod = 1f;
-    public GameObject enemy;
+    public int fala;
     void Start()
     {
-
+        fala = 1;
     }
 
 
     void Update()
     {
-        time += Time.deltaTime;
-
-        if (time >= interpolationPeriod)
-        {
-            time = 0.0f;
-            float lowestPoint = transform.position.y - 30;
-            float highestPoint = transform.position.y + 30;
-            Instantiate(enemy, new Vector2(Random.Range(lowestPoint, highestPoint), Random.Range(lowestPoint, highestPoint)), transform.rotation);
-        }
 
     }
 }
